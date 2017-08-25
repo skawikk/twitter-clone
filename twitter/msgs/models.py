334c7edeff7176f138bdf36a90f8c440e3b7630e
Maciej Hrehorowicz <hrehorowicz.maciej@gmail.com>
2017-08-25 14:23:44 +0200

    Created model Msg

--- twitter/msgs/models.py
+++ twitter/msgs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
+
+class Msg(models.Model):
+    sender = models.ForeignKey(User)
+    reciever = models.ForeignKey(User)
+    content = models.TextField()
+    is_read = models.BooleanField()
+
 
 # Create your models here.
